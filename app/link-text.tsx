eed764b408d5c4bdd44d3aa225b7e22a1ee65aac
Alexander Kronfeld <alexander@testparty.ai>
2025-03-19 18:13:52 -0400

    amk - link test

--- app/link-text.tsx
+++ app/link-text.tsx
@@ -12,20 +12,13 @@ export default function LinkTextScreen() {
             </ThemedView>
             <ThemedText style={styles.margin}>Default Passing Cases</ThemedText>
             <ThemedView style={styles.container}>
-                <ExternalLink href='/'>External Link passes via text</ExternalLink>
-                <ExternalLink href='/' accessibilityLabel='Passing External Link' />
-                <Link href='/'>Internal Link passes via text</Link>
-                <Link href='/' accessibilityLabel='Passing Internal Link' />
+                <Link href={'/'} accessibilityLabel='Link to Home Page'>Passing Internal Link</Link>
+                <ExternalLink href={'/'} accessibilityLabel='External Link'>Passing External Link</ExternalLink>
             </ThemedView>
-            <ThemedText style={styles.margin}>Case 1: Expo Links fail with no text or Accessibilty Label</ThemedText>
+            <ThemedText style={styles.margin}>Case 1: Accessibility label must be unique.  The Expo Links below have the same accessibility label so they both fail the check. </ThemedText>
             <ThemedView style={styles.container}>
-                <Link href={"/"} />
-                <ExternalLink href={"/"} />
-            </ThemedView>
-            <ThemedText style={styles.margin}>Case 2: Accessibility label must be unique.  The Expo Links below have the same accessibility label so they both fail the check. </ThemedText>
-            <ThemedView style={styles.container}>
-                <Link href={"/"} accessibilityLabel="I fail due to duplicate label">Internal</Link>
-                <ExternalLink href={"/"} accessibilityLabel="I fail due to duplicate label">External</ExternalLink>
+                <Link href={'/'} accessibilityLabel="I fail due to duplicate label">Internal Duplicate</Link>
+                <ExternalLink href={'/'} accessibilityLabel="I fail due to duplicate label">External Duplicate</ExternalLink>
             </ThemedView>
         </ThemedView>
     )
