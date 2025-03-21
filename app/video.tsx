9dcdb61674958c8ce420bda7baff6e026668cb48
Christian <167792134+christian-testparty@users.noreply.github.com>
2025-03-21 14:16:44 -0400

    added expo-av video element and cleaned up exisitng test cases

--- app/video.tsx
+++ app/video.tsx
@@ -1,42 +1,150 @@
-import { useEvent } from "expo";
 import { useVideoPlayer, VideoView } from "expo-video";
-import { StyleSheet, View, Button } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Video } from "expo-av";
+import { useRef } from "react";
 
-const videoSource = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+const videoSource: string =
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
-export default function VideoScreen() {
-    const player = useVideoPlayer(videoSource, (player) => {
+export default function VideoScreen(): JSX.Element {
+    const videoRef = useRef(null);
+
+    const player1 = useVideoPlayer(videoSource, (player) => {
         player.loop = true;
-        player.play();
     });
 
-    const { isPlaying } = useEvent(player, "playingChange", {
-        isPlaying: player.playing,
+    const player2 = useVideoPlayer(videoSource, (player) => {
+        player.loop = true;
+    });
+
+    const player3 = useVideoPlayer(videoSource, (player) => {
+        player.loop = true;
+    });
+
+    const player4 = useVideoPlayer(videoSource, (player) => {
+        player.loop = true;
+    });
+
+    const player5 = useVideoPlayer(videoSource, (player) => {
+        player.loop = true;
+    });
+
+    const player6 = useVideoPlayer(videoSource, (player) => {
+        player.loop = true;
     });
 
     return (
         <ThemedView style={styles.screen}>
-            <ThemedText style={styles.title}>Testing "video" rule</ThemedText>
-            <View style={styles.videoContainer}>
-                <VideoView
-                    style={styles.video}
-                    player={player}
-                    allowsFullscreen
-                    accessibilityLabel="Funny bunny"
-                    accessibilityHint="A bunny hopping around"
-                    accessible={true}
-                    allowsPictureInPicture
-                />
-            </View>
-            <View style={styles.controlsContainer}>
-                <Button
-                    title={isPlaying ? "Pause" : "Play"}
-                    onPress={() => (isPlaying ? player.pause() : player.play())}
-                    color="#007AFF"
-                />
-            </View>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <ThemedView style={styles.container}>
+                    <ThemedText type="title" style={styles.title}>
+                        Testing Video Accessibility Label
+                    </ThemedText>
+                </ThemedView>
+
+                {/* Pass Case: expo-av */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: expo-av
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <Video
+                            source={{ uri: videoSource }}
+                            ref={videoRef}
+                            useNativeControls
+                            style={styles.video}
+                            accessibilityLabel="Funny bunny expo-av"
+                        />
+                    </View>
+                </View>
+
+                {/* Pass Case: expo-video */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: expo-video
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView style={styles.video} player={player1} />
+                    </View>
+                </View>
+
+                {/* Pass Case: accessibilityLabel and accessible={true} */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: accessibilityLabel and accessible={"{true}"}
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView
+                            style={styles.video}
+                            player={player2}
+                            accessibilityLabel="Funny bunny"
+                            accessible={true}
+                        />
+                    </View>
+                </View>
+
+                {/* Pass Case: accessibilityLabel and accessible={false} */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: accessibilityLabel and accessible={"{false}"}
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView
+                            style={styles.video}
+                            player={player3}
+                            accessibilityLabel="Funny bunny"
+                            accessible={false}
+                        />
+                    </View>
+                </View>
+
+                {/* Pass Case: missing accessible=true */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: missing accessible={"{true}"}
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView
+                            style={styles.video}
+                            player={player4}
+                            accessibilityLabel="Funny bunny"
+                        />
+                    </View>
+                </View>
+
+                {/* Pass Case: existing testID */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Pass Case: existing testID
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView
+                            style={styles.video}
+                            player={player5}
+                            accessibilityLabel="Funny bunny"
+                            accessible={true}
+                            testID="video-element"
+                        />
+                    </View>
+                </View>
+
+                {/* Fail Case: missing accessibilityLabel */}
+                <View>
+                    <ThemedText style={styles.margin}>
+                        Fail Case: missing accessibilityLabel and accessible=
+                        {"{true}"}
+                    </ThemedText>
+                    <View style={styles.videoContainer}>
+                        <VideoView
+                            style={styles.video}
+                            player={player6}
+                            accessible={true}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
         </ThemedView>
     );
 }
@@ -45,15 +153,19 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         backgroundColor: "#f8f9fa",
-        alignItems: "center",
+    },
+    scrollContainer: {
+        flexGrow: 1,
         padding: 20,
     },
+    container: {
+        marginTop: 10,
+        marginBottom: 10,
+        display: "flex",
+        alignItems: "center",
+    },
     title: {
-        fontSize: 22,
-        fontWeight: "bold",
-        marginBottom: 20,
         textAlign: "center",
-        color: "#333",
     },
     videoContainer: {
         width: "100%",
@@ -64,14 +176,13 @@ const styles = StyleSheet.create({
         backgroundColor: "#000",
         justifyContent: "center",
         alignItems: "center",
+        marginBottom: 20,
     },
     video: {
         width: "100%",
         height: "100%",
     },
-    controlsContainer: {
-        marginTop: 20,
-        width: "80%",
-        alignItems: "center",
+    margin: {
+        margin: 5,
     },
 });
